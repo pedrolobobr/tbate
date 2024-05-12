ee880585349bc4bf722e7415a82b48390dd08377
Pedro Henrique <pgames658@gmail.com>
2024-05-12 05:09:44 -0300

    teste2

--- while.py
+++ while.py
@@ -1,4 +1,4 @@
 x = 1
 while x < 481:
-    print(f'<a href="capitulos/{x}.html"> <button>Capítulo {x}</button></a>')
+    print(f'<a href="capitulos/{x}.html"> <button class="button-54" role="button">Capítulo {x}</button></a>')
     x = x + 1
