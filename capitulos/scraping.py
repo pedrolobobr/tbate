dc9419b3112ea3e946c7f1c138b004a19c9406bf
Pedro Henrique <pgames658@gmail.com>
2024-05-12 19:10:01 -0300

    teste4

--- capitulos/scraping.py
+++ capitulos/scraping.py
@@ -27,7 +27,7 @@ for i in range(400, 481):
 
     # Escrever o conteúdo HTML no arquivo
   with open(f'{i}.html', 'w', encoding="utf-8") as arquivo:
-      arquivo.write(f'<style>{style1}</style><body style="background-color: #222222; color: white;">{titulo}<h2 style="text-align: center;">{chaptername}</h2><p style="text-align: center;">Lançamento {data}</p>{noticia}</body>')
+      arquivo.write(f'<style>{style1}</style><body style="background-color: #222222; color: white; margin: 25px;">{titulo}<h2 style="text-align: center;">{chaptername}</h2><p style="text-align: center;">Lançamento {data}</p>{noticia}</body>')
   print(f"Arquivo {i} criado com sucesso!")
 
 
